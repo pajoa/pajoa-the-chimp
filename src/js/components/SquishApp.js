0fe499025f84f6c0f119d21c2b92e23dea63737f
Anita Amini <baadge29@yahoo.co.uk>
2015-04-13 16:53:43 +0100

    1st react components

--- src/js/components/SquishApp.js
+++ src/js/components/SquishApp.js
@@ -1,14 +1,21 @@
 var React = require("react");
+var Notes = require("./sections/Notes");
 
 var SquishApp = React.createClass({
 	
+    getInitialState: function() {
+        return {
+            router: "notes"
+        };
+    },
+    
 	render: function() {
-		return(
-			<div >
-				<h1>I hate React</h1>
-			</div>
-		);
-	}
+        if (this.state.router === "notes") {
+            return(
+                <Notes />
+            );
+        }
+    }
 });
 
 module.exports = SquishApp;
