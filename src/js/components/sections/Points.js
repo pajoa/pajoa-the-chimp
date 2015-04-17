93154b4cf405c1b3b752a1e3c37cbd601b9ea834
amilvasishtha <amil_v@msn.com>
2015-04-17 16:28:55 +0100

    main.css and points.js

--- src/js/components/sections/Points.js
+++ src/js/components/sections/Points.js
@@ -3,12 +3,16 @@ var ActionCreators = require("../../actions/ActionCreators");
 var Points = React.createClass({
     
     render: function() {
-    console.log("this is points: ", this.props);
+    console.log("this is points: ", this);
         return (
             <div>
             	<h1>Points Dashboard</h1>
-            		<h4>Welcome to your Points Dashboard, where you can keep an eye on how well you are doing</h4>
-            	<img src="../img/trophy.png" className="points" />
+            		<h4 className="pointsTitle">Welcome to your Points Dashboard, where you can keep an eye on how well you are doing</h4>
+                    <img src="../img/trophy.png" className="points" />
+            	<div className="myPoints">
+                    <h2>105</h2>
+                    <h4>POINTS</h4>
+                </div>
             </div>
         );
     }
