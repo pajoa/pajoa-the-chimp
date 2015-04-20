99aa7aa86cb80540c52c5a370f0cb4bc476c8885
Anita Amini <baadge29@yahoo.co.uk>
2015-04-20 17:38:34 +0100

    debugging for heroku

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ var User = mongoose.model('User', userSchema);
 
 server.connection({
 	port: process.env.PORT || 8000,
-    host: "0.0.0.0" || "localhost"
+    host: "0.0.0.0"
 });
 
 server.register([require('bell'), require('hapi-auth-cookie')], function(err){
--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var server = require("./api/server");
 
 server.server.start(function() {
 	console.log("Server running at " + server.server.info.uri);
-	var cron = require("./api/cron.js"); //schedule
+//	var cron = require("./api/cron.js"); //schedule
 });
