e8394e87e4e02f652576f8addf54d55631ed5489
perborgen <perhb@hotmail.com>
2015-04-20 17:16:07 +0100

    moment changes

--- src/js/components/sections/Notes.js
+++ src/js/components/sections/Notes.js
@@ -2,7 +2,8 @@ var React = require("react");
 var ActionCreators = require("../../actions/ActionCreators");
 var Router = require('react-router'); // or var Router = ReactRouter; in browsers
 var Link = Router.Link;
- 
+var moment = require("moment");
+
  function getStateFromStore() {
       
     var route = AppStore.getRoute(); 
@@ -33,21 +34,26 @@ var Notes = React.createClass({
     render: function() {
         console.log('data in notes: ',this.props.data);
         var self = this;
+        var today = moment().format("dddd, MMMM Do YYYY");
+        console.log(today.unix());
         var notes = this.props.data.map(function(note){
-      maxLength = 100;
-                var ret = note.text;
-                    if (ret.length > maxLength) {
-                        ret = ret.substr(0,maxLength-1) + "...";
-                    }
-
-                return (
-                    <div key={note.id} className="col-md-4 col-sm-6 col-xs-12 col-lg-3">
-                        <div className="notesCard">
-                            <div className="notesCardTitle"><h3><a className="noteTitle" href={"/#/" + note.id} name={note.id}>{note.title}</a></h3></div>
-                            <div className="notesCardText"><p>{ret}</p></div>
-                        </div>
+            var deadlineStamps = note.deadlines.map(function(deadline){
+                
+            });
+            maxLength = 100;
+            var ret = note.text;
+            if (ret.length > maxLength) {
+                ret = ret.substr(0,maxLength-1) + "...";
+            }
+            return (
+                <div key={note.id} className="col-md-4 col-sm-6 col-xs-12 col-lg-3">
+                    <div className="notesCard">
+                        <div className="notesCardTitle">
+                        <h3><a className="noteTitle" href={"/#/" + note.id} name={note.id}>{note.title}</a></h3></div>
+                        <div className="notesCardText"><p>{ret}</p></div>
                     </div>
-                );
+                </div>
+            );
         });
         
         return (
