9102f9e1922f62075f4ce84c895c558c3c824e89
Jasonspd <jasoncluu@googlemail.com>
2015-05-01 13:49:37 +0100

    agenda...

--- api/agenda.js
+++ api/agenda.js
@@ -2,11 +2,54 @@ var server = require('./server.js');
 var nodemailer = require('nodemailer');
 var moment  = require('moment');
 var today = moment().format("YYYY MM DD");
+var config = require("./config.js");
+
+
+// server.User.find(function(err,users){
+// 	users.forEach(function(user){
+// 			user.notes.forEach(function(note){
+// 			note.deadlines.forEach(function(deadline){
+//   				if (deadline.day === today && deadline.points == 0){
+//         			console.log("Found: " + user.email + " with note: " + note.title);
+//         			Sendmail(user.email, note.title, users.length);
+//         		}
+//         	});
+//         });
+//     });
+// });
+
+// var Sendmail = function(email, title, users) {
+
+//     var transporter = nodemailer.createTransport({
+//         service: 'Gmail',
+//         auth: {
+//             user: config.reminder.email,
+//             pass: config.reminder.password
+//         }
+//     });
+
+//     var link = "http://pajoa.herokuapp.com/"
+
+//     var mailOptions = {
+// 		from: 'Squish Reminder <contactsquish@gmail.com>',
+// 		to: email,
+// 		subject: 'Revise today! ✔',
+// 		text: 'Revise today and stay on top of your notes. You will get a point if you login today: ' + link + ' Your notes: ' + title,
+// 		html: 'Revise today and stay on top of your notes! You will get a point if you login today: <a href=' + link + '>' + title + '</a></b>'
+//     };
+    
+//     transporter.sendMail(mailOptions, function(error, info){
+//     	if(error){
+//     		console.log(error);
+//     	}else{
+//     		console.log('Message sent: ' + info.response);
+//     	}
+//     });
+// };
 
 function agendaStart() {
 
 	console.log("agenda started...");
-	var config = require("./config.js");
 
 	var Agenda = require('agenda');
 	var agenda = new Agenda({db: { address: config.db.dburl}});
@@ -75,7 +118,7 @@ function agendaStart() {
 		console.log("its's done");
 	});
 
-	agenda.every('00 10 * * *', 'checking deadline at 10am');
+	agenda.every('00 10 * * *', 'checking deadline');
 	 
 	agenda.start();
 };
